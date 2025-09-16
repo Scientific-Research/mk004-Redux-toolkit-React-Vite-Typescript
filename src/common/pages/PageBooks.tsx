9765ce56939de258e9bb500302bbe67a1da9b907
Maximilian <scientificresearch309@gmail.com>
2025-09-16 17:40:34 +0200

    the add and substract are working mit typscript but without CSS

--- src/common/pages/PageBooks.tsx
+++ src/common/pages/PageBooks.tsx
@@ -1,7 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { add, subtract } from '../../features/cart/cartSlice';
+import { RootState } from '../../app/store';
+
 export const PageBooks = () => {
-        return (
-            <>
-                <p>Welcome to the Books page.</p>
-            </>
-        );
-    };
+  const dispatch = useDispatch();
+  const selectCart = (state: RootState) => state.cart;
+  const { count } = useSelector(selectCart);
+
+  const handleSubtractOne = () => {
+    dispatch(subtract());
+  };
+
+  const handleAddOne = () => {
+    dispatch(add());
+  };
+
+  return (
+    <div className="pageBooks">
+      <p>Welcome to the Books page.</p>
+      <button onClick={handleSubtractOne}>Subtract One</button>
+      <button onClick={handleAddOne}>Add One</button>
+      <hr />
+      <span>{count}</span>
+    </div>
+  );
+};
